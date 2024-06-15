f228aa9c6f420516be1c68ae9e62c0ed6993db11
altugakinci <altug.akinci@hotmail.com>
2024-06-15 13:40:50 +0300

    2 more solutions on 15.06.2024

---
+++ LeetCode_Problems/Easy/P_58_Length_of_Last_Word.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode_Problems.Easy;
+
+public class P_58_Length_of_Last_Word
+{
+    public int LengthOfLastWord(string s)
+    {
+        int index = 0;
+        int counter = 0;
+        int lastWord = 0;
+
+        while (index < s.Length)
+        {
+            if (s[index] == ' ')
+            {
+                if (counter != 0)
+                    lastWord = counter;
+                counter = 0;
+            }
+            else
+                counter++;
+
+            index++;
+        }
+
+        if (counter != 0)
+            lastWord = counter;
+
+        return lastWord;
+    }
+}
---
+++ LeetCode_Problems/Easy/P_66_Plus_One.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode_Problems.Easy;
+
+public class P_66_Plus_One
+{
+    public int[] PlusOne(int[] digits)
+    {
+        int index = digits.Length - 1;
+
+        int curr = digits[index];
+
+        if (curr != 9)
+        {
+            digits[index] = curr + 1;
+            return digits;
+        }
+        else
+        {
+            int carry = 1;
+            do
+            {
+                curr = digits[index];
+
+                if (curr + carry == 10)
+                {
+                    carry = 1;
+                    digits[index] = 0;
+                }
+                else
+                {
+                    digits[index] = curr + carry;
+                    carry = 0;
+                }
+
+                index--;
+            } while (carry != 0 && index != -1);
+
+            // If there is still a carry, then create a n+1 array.
+            if (carry != 0)
+            {
+                int[] temp = new int[digits.Length + 1];
+                temp[0] = 1;
+                for (int i = 1; i < temp.Length; i++)
+                {
+                    temp[i] = digits[i - 1];
+                }
+                return temp;
+            }
+        }
+
+        return digits;
+    }
+}
