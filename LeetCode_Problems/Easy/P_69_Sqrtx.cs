75c0751d631ca184a0275aff89dfc37e72e54b3e
altugakinci <altug.akinci@hotmail.com>
2024-06-23 15:25:42 +0300

    1 more solution on 23.06.2024

---
+++ LeetCode_Problems/Easy/P_69_Sqrtx.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode_Problems.Easy;
+
+public class P_69_Sqrtx
+{
+    public int MySqrt(int x)
+    {
+        if (x == 1)
+            return 1;
+
+        long l = 0, r = x, mean, buffer, prod;
+
+        buffer = r;
+
+        while (true)
+        {
+            mean = (l + r) / 2;
+
+            if (mean == buffer)
+                return (int)mean;
+
+            buffer = mean;
+
+            prod = mean * mean;
+
+            if (prod < 0)
+            {
+                r = mean;
+                continue;
+            }
+
+            if (prod == x)
+                return (int)mean;
+
+            if (prod > x)
+                r = mean;
+            else
+                l = mean;
+        }
+    }
+}
