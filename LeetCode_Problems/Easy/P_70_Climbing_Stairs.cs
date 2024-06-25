7cc0ad13e79841ac1b64bf1a89ed0de32fbb979d
altugakinci <altug.akinci@hotmail.com>
2024-06-25 15:42:56 +0300

    1 more solution on 25.06.2024

---
+++ LeetCode_Problems/Easy/P_70_Climbing_Stairs.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode_Problems.Easy;
+
+public class P_70_Climbing_Stairs
+{
+    public int ClimbStairs(int n)
+    {
+        if (n == 1)
+            return 1;
+
+        int a = 1, b = 1;
+        int c = 0;
+
+        for (int i = 2; i < n + 1; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+}
