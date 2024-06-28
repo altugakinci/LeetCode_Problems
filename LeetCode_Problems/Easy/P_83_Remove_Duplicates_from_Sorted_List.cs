071b90f196c8569d13b3b1489eae61d26b42c64d
altugakinci <altug.akinci@hotmail.com>
2024-06-28 08:18:13 +0300

    2 more solutions on 28.06.2024

---
+++ LeetCode_Problems/Easy/P_83_Remove_Duplicates_from_Sorted_List.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode_Problems.Easy;
+
+internal class P_83_Remove_Duplicates_from_Sorted_List
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        if (head is null)
+            return null;
+
+        ListNode root = head;
+
+        while (root.next != null)
+        {
+            if (root.val == root.next.val)
+            {
+                root.next = root.next.next;
+            }
+            else
+            {
+                root = root.next;
+            }
+        }
+
+        return head;
+    }
+}
+
+
---
+++ LeetCode_Problems/Easy/P_88_Merge_Sorted_Array.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode_Problems.Easy;
+
+internal class P_88_Merge_Sorted_Array
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int nums1Index = m;
+        bool inserted;
+
+        for (int i = 0; i < n; i++)
+        {
+            inserted = false;
+
+            for (int j = 0; j < nums1Index; j++)
+            {
+                if (nums1[j] > nums2[i])
+                {
+                    InsertToArray(nums1, j, nums2[i]);
+                    nums1Index++;
+                    inserted = true;
+                    break;
+                }
+            }
+
+            if (!inserted)
+            {
+                InsertToArray(nums1, nums1Index, nums2[i]);
+                nums1Index++;
+            }
+        }
+    }
+
+    public void InsertToArray(int[] array, int index, int value)
+    {
+        for (int i = array.Length - 1; i > index; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[index] = value;
+    }
+}
