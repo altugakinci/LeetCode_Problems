ec00d24be86d7faa14f363bc50fa7385ab71a12f
altugakinci <altug.akinci@hotmail.com>
2024-06-28 08:18:34 +0300

    Added linkedlist and array helpers

---
+++ LeetCode_Problems/Helpers/Helpers.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode_Problems.Easy;
+
+namespace LeetCode_Problems.Helpers;
+
+public class Helpers
+{
+    public static void PrintLinkedList(ListNode node)
+    {
+        while (node != null)
+        {
+            Console.WriteLine(node.val);
+            node = node.next;
+        }
+    }
+
+    public static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
